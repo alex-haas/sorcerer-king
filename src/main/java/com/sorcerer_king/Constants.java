8febcae340783fa22fe13abdd88e27b0677aa156
Alexander Haas <alexanderhaas@live.de>
2022-10-05 11:38:22 +0200

    initial commit

---
+++ src/main/java/com/sorcerer_king/Constants.java
@@ -0,0 +1,5 @@
+package com.sorcerer_king;
+
+public class Constants {
+    public static final String MOD_ID = "sorcerer-king";
+}
---
+++ src/main/java/com/sorcerer_king/SorcererKing.java
@@ -0,0 +1,14 @@
+package com.sorcerer_king;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SorcererKing implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world from Server!");
+    }
+}
