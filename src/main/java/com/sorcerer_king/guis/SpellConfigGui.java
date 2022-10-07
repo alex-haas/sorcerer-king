b019ebd5f42ca9db80d1209c072747446edfcfc6
Alexander Haas <alexanderhaas@live.de>
2022-10-07 14:32:04 +0200

    # added translations for items and blocks

--- src/main/java/com/sorcerer_king/guis/SpellConfigGui.java
+++ src/main/java/com/sorcerer_king/guis/SpellConfigGui.java
@@ -2,11 +2,15 @@ package com.sorcerer_king.guis;
 
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.WGridPanel;
+import io.github.cottonmc.cotton.gui.widget.WLabel;
+import net.minecraft.text.Text;
 
 public class SpellConfigGui extends LightweightGuiDescription {
     public SpellConfigGui() {
-        WGridPanel mainPanel = new WGridPanel();
-        setRootPanel(mainPanel);
-        mainPanel.setSize(300, 200);
+        WGridPanel root = new WGridPanel();
+        root.setSize(300, 200);
+        WLabel label = new WLabel(Text.translatable("gui.spell_config.title"));
+        root.add(label, 1, 1);
+        setRootPanel(root);
     }
 }
